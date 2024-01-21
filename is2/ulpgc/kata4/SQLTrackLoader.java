f5ba2d98a99bc6ea692aa3fa3150d63de20e3e9e
AVR21 <alejandroviera12@gmail.com>
2024-01-21 15:44:58 +0000

    Added Track record and TrackLoader interface.

---
+++ is2/ulpgc/kata4/SQLTrackLoader.java
@@ -0,0 +1,12 @@
+package is2.ulpgc.kata4;
+
+import java.util.List;
+
+public class SQLTrackLoader implements TrackLoader{
+
+
+    @Override
+    public List<Track> load() {
+        return null;
+    }
+}
